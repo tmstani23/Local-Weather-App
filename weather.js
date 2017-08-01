2fa3ba02f967134604456a71165f8679ec22c86b
tmstani23 <tmstani23@gmail.com>
2017-08-01 16:16:10 -0500

    User Location Update
    Created a button that gets the user location on click.  Added the html
    button and necessary paragraph elements.

--- weather.js
+++ weather.js
@@ -1,10 +1,23 @@
 
-if (typeof jQuery != 'undefined') {
+//get user location
+var locElement = document.getElementById("userLoc");
 
-    alert("jQuery library is loaded!");
-
-} else{
-
-    alert("jQuery library is not found!");
-
+function getLoc() {
+    //if user location is supported:
+    if (navigator.geolocation) {
+        //get current user location and run showPosition function
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    //else display error message to user
+    else {
+        locElement.innerHTML = "Geolocation is not supported by this browser.  Activate to continue..."
+    }
 }
+//show user position
+function showPosition(position) {
+    locElement.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+//load jquery data
+//temp-humidity
+//add button that changes f to celsius
+//add icon that changes depending on the weather
