b5a6cf524f780ce603d3864a3d19506bd20ad1b8
tmstani23 <tmstani23@gmail.com>
2018-07-15 16:23:34 -0500

    Update 3
    Added functions to main app component to check geolocation, compose request url, make request and save data.

--- weather-new.jsx
+++ weather-new.jsx
@@ -1,4 +1,14 @@
 
+// function MakeReq (lat, long) {
+//     var mainBlock = "https://fcc-weather-api.glitch.me/api/current?";
+//     var latBlock = "lat=";
+//     var lonBlock = "&lon=";
+   
+//     //concatenate all the variables to make the request string:
+//   var request = mainBlock + latBlock + lat + lonBlock + long;
+// }
+
+
 
 class App extends React.Component {
     
@@ -9,13 +19,47 @@ class App extends React.Component {
             data: ""
         }
     }
-    
-    getData = () => console.log("hello");
+
+    checkGeo = () => { 
+        if (navigator.geolocation) {
+        //get current user location and run showPosition function
+        //navigator.geolocation.getCurrentPosition(getLatitude(position));
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.composeRequest(position.coords.latitude, position.coords.longitude);
+            });
+        }
+    //else display error message to user
+        else {
+            console.log("nogeo");
+        }
+    }
+    fetchData = (request) => {
+        
+        fetch(request)
+            .then(function(response) {
+                
+                return response.json();
+            })
+            .then(function(myJson) {
+                console.log(myJson);
+                
+            });
+        
+    }
+    composeRequest = (latitude, longitude) => {
+    const request = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+    //console.log(request);
+    return this.fetchData(request);
+    } 
+
+    componentDidMount () {
+        this.checkGeo();
+    }  
 
     render () {
         return (
             <div>
-                <h1>{this.getData()}</h1> 
+                <h1>{this.state.data}</h1> 
             </div>
                
         )
