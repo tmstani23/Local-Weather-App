420c89decec459e8534b516b4a51e7eeecaa0df8
tmstani23 <tmstani23@gmail.com>
2018-07-19 18:23:30 -0500

    Update 7
    Added change between celsius and temperature button.  The button updates all temps in the ui on click.  Adjusted request so teh array is populated with temp, tempmin, temp max as well as the rest of the json data object to handle referincing when updating state between parent and child.  tempChange function is fed as props to the child datagrid component and used to handle the on click events.  The function updates the temperature attributes of the state and these are fed as props to the DataGrid component for rendering.  *Need to add conditionality to switch between F/C as now it is just switching to F then adding the calculation on every click.

--- weather-new.jsx
+++ weather-new.jsx
@@ -28,18 +28,27 @@ class Loading extends React.Component {
 
 function DataGrid(props) {
     //console.log(props);
-    console.log(typeof props.data.iconDescription )
-        
+    //console.log(typeof props.data.iconDescription )
+    
     return (
         <ul className="data-grid">
             {/* Here each piece of the data is mapped to a list element and displayed */}
-            <li>{ props.data.city }, { props.data.country }</li>
+            {/* <li>{ props.data.city }, { props.data.sys.country }</li>
             <li>Current Temp: { props.data.temp } </li>
             <li>Temperature Range: { props.data.tempMin } - { props.data.tempMax }</li>
             <li> <img src={ props.data.icon }/> </li>
             <li> { props.data.iconDescription.toUpperCase() } </li>
             <li> Humidity: { props.data.humidity } </li>
-            <li> Visibility: { (props.data.visibilityMiles / 5280).toFixed(2) } miles</li>
+            <li> Visibility: { (props.data.visibilityMiles / 5280).toFixed(2) } miles</li> */}
+
+            <li onClick={props.handleChildClick}>Farenheit/Celsius</li>
+            <li>{ props.data.name }, { props.data.sys.country }</li>
+            <li >Current Temp: { props.temp } </li>
+            <li>Temperature Range: { props.tempMin } - { props.tempMax }</li>
+            <li> <img src={ props.data.weather[0].icon }/> </li>
+            <li> { props.data.weather[0].description.toUpperCase() } </li>
+            <li> Humidity: { props.data.main.humidity } </li>
+            <li> Visibility: { (props.data.visibility / 5280).toFixed(2) } miles</li>
         </ul>
     )
     
@@ -52,6 +61,8 @@ class DataComponent extends React.Component {
 
         this.state = {
             data: [ {} ],
+            tempMin: null,
+            tempMax: null,
             error: "",
             loading: true
         }
@@ -75,33 +86,50 @@ class DataComponent extends React.Component {
         this.setState( {loading: true} );
         fetch(request)
             .then( (response) =>  response.json() )
+            // .then( (dataObj) => this.setState( 
+            //     {   data: [ {temp: dataObj.main.temp,
+            //             icon: dataObj.weather[0].icon,
+            //             iconDescription: dataObj.weather[0].description,
+            //             tempMin: dataObj.main.temp_min,
+            //             tempMax: dataObj.main.temp_max,
+            //             humidity: dataObj.main.humidity,
+            //             visibilityMiles: dataObj.visibility,
+            //             city: dataObj.name,
+            //             country: dataObj.sys.country   
+            //         } ]
+            //     }
+            // ) )
             .then( (dataObj) => this.setState( 
-                {   data: [ {temp: dataObj.main.temp,
-                        icon: dataObj.weather[0].icon,
-                        iconDescription: dataObj.weather[0].description,
-                        tempMin: dataObj.main.temp_min,
-                        tempMax: dataObj.main.temp_max,
-                        humidity: dataObj.main.humidity,
-                        visibilityMiles: dataObj.visibility,
-                        city: dataObj.name,
-                        country: dataObj.sys.country   
-                    } ]
+                {   data: [dataObj],
+                    temp: dataObj.main.temp,
+                    tempMin: dataObj.main.temp_min,
+                    tempMax: dataObj.main.temp_max
                 }
             ) )
             //.then( () => console.log(this.state.data[0]) )
             .then( () => this.setState( { loading: false } ) )
-            .catch(error => this.setState( { error, isLoading: false } ) );
-}
+            .catch(error => this.setState( { error, loading: false } ) );
+    }
     composeRequest = (latitude, longitude) => {
-    const request = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
-    console.log(request);
-    return this.fetchData(request);
+        const request = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+        console.log(request);
+        return this.fetchData(request);
     } 
 
     componentDidMount () {
         this.checkGeo();
         
     }  
+    tempChange = () => {
+        //console.log(this.state.data);
+        //need to add cels/f condition checks to state
+        const changeTemp = [this.state.temp, this.state.tempMax, this.state.tempMin].map((acc) => acc * 1.8 + 32)
+        console.log(changeTemp);
+        return this.setState({ temp: changeTemp[0].toFixed(2), 
+            tempMin: changeTemp[1].toFixed(2),
+            tempMax:  changeTemp[2].toFixed(2) 
+        })
+    }
 
     render () {
         return (
@@ -110,7 +138,7 @@ class DataComponent extends React.Component {
                 { this.state.loading === true 
                 ? <Loading />
                 // Else display a header with the current language and the RepoGrid component
-                : <DataGrid data = {this.state.data[0]} />
+                : <DataGrid data = {this.state.data[0]} handleChildClick= {this.tempChange} temp = {this.state.temp} tempMin = {this.state.tempMin} tempMax = {this.state.tempMax}/>
                 
                 }
                 
