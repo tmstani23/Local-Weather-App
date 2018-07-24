4f0fc2ea194147d3f94c473fc2d82e8c5420cd8f
tmstani23 <tmstani23@gmail.com>
2018-07-23 19:28:20 -0500

    Update 8
    Updated tempChange method to switch between celsius and farenheit and do the calculation.  Updated datagrid display to take the new tempchange function as a prop and display the currect temperature state accross all temp display elements.  Added temp change button to datagrid display with dynamically changing text based on its temp state.  Began work on refactoring and cleaning up the code.

--- weather-new.jsx
+++ weather-new.jsx
@@ -27,21 +27,9 @@ class Loading extends React.Component {
 }
 
 function DataGrid(props) {
-    //console.log(props);
-    //console.log(typeof props.data.iconDescription )
-    
     return (
         <ul className="data-grid">
-            {/* Here each piece of the data is mapped to a list element and displayed */}
-            {/* <li>{ props.data.city }, { props.data.sys.country }</li>
-            <li>Current Temp: { props.data.temp } </li>
-            <li>Temperature Range: { props.data.tempMin } - { props.data.tempMax }</li>
-            <li> <img src={ props.data.icon }/> </li>
-            <li> { props.data.iconDescription.toUpperCase() } </li>
-            <li> Humidity: { props.data.humidity } </li>
-            <li> Visibility: { (props.data.visibilityMiles / 5280).toFixed(2) } miles</li> */}
-
-            <li onClick={props.handleChildClick}>Farenheit/Celsius</li>
+            <button onClick={props.handleChildClick}>&deg;{props.mUnits}</button>
             <li>{ props.data.name }, { props.data.sys.country }</li>
             <li >Current Temp: { props.temp } </li>
             <li>Temperature Range: { props.tempMin } - { props.tempMax }</li>
@@ -64,41 +52,20 @@ class DataComponent extends React.Component {
             tempMin: null,
             tempMax: null,
             error: "",
-            loading: true
+            loading: true,
+            isCelsius: "F"
         }
-        //this.fetchData = this.fetchData.bind(this);
     }
 
     checkGeo = () => { 
-        if (navigator.geolocation) {
-        //get current user location and run showPosition function
-            navigator.geolocation.getCurrentPosition((position) => {
-                this.composeRequest(position.coords.latitude, position.coords.longitude);
-            });
-        }
-    //else display error message to user
-        else {
-            console.log("nogeo");
-        }
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.composeRequest(position.coords.latitude, position.coords.longitude);
+        }) 
     }
     fetchData = (request) => {
-        //console.log(request);
         this.setState( {loading: true} );
         fetch(request)
             .then( (response) =>  response.json() )
-            // .then( (dataObj) => this.setState( 
-            //     {   data: [ {temp: dataObj.main.temp,
-            //             icon: dataObj.weather[0].icon,
-            //             iconDescription: dataObj.weather[0].description,
-            //             tempMin: dataObj.main.temp_min,
-            //             tempMax: dataObj.main.temp_max,
-            //             humidity: dataObj.main.humidity,
-            //             visibilityMiles: dataObj.visibility,
-            //             city: dataObj.name,
-            //             country: dataObj.sys.country   
-            //         } ]
-            //     }
-            // ) )
             .then( (dataObj) => this.setState( 
                 {   data: [dataObj],
                     temp: dataObj.main.temp,
@@ -106,7 +73,6 @@ class DataComponent extends React.Component {
                     tempMax: dataObj.main.temp_max
                 }
             ) )
-            //.then( () => console.log(this.state.data[0]) )
             .then( () => this.setState( { loading: false } ) )
             .catch(error => this.setState( { error, loading: false } ) );
     }
@@ -121,29 +87,46 @@ class DataComponent extends React.Component {
         
     }  
     tempChange = () => {
-        //console.log(this.state.data);
-        //need to add cels/f condition checks to state
-        const changeTemp = [this.state.temp, this.state.tempMax, this.state.tempMin].map((acc) => acc * 1.8 + 32)
-        console.log(changeTemp);
-        return this.setState({ temp: changeTemp[0].toFixed(2), 
+        //if is celsius set temp settings to celsius from data object:
+        if (this.state.isCelsius == "F") {
+            return this.setState( 
+            {   
+                temp: this.state.data[0].main.temp, 
+                tempMin: this.state.data[0].main.temp_min,
+                tempMax: this.state.data[0].main.temp_max,
+                isCelsius: "C"
+            } )
+        }
+        else {
+            const changeTemp = [this.state.temp, this.state.tempMax, this.state.tempMin].map((acc) => acc * 1.8 + 32)
+            //console.log(changeTemp);
+            return this.setState({ temp: changeTemp[0].toFixed(2), 
             tempMin: changeTemp[1].toFixed(2),
-            tempMax:  changeTemp[2].toFixed(2) 
+            tempMax:  changeTemp[2].toFixed(2),
+            isCelsius: "F"
         })
+        }
+        
     }
 
     render () {
         return (
             <div>
-                {/* <h1>{this.state.temp}</h1> */}
-                { this.state.loading === true 
+                { this.state.loading === true
                 ? <Loading />
                 // Else display a header with the current language and the RepoGrid component
-                : <DataGrid data = {this.state.data[0]} handleChildClick= {this.tempChange} temp = {this.state.temp} tempMin = {this.state.tempMin} tempMax = {this.state.tempMax}/>
+                : <DataGrid data = {this.state.data[0]} handleChildClick= {this.tempChange} 
+                temp = {this.state.temp} tempMin = {this.state.tempMin} 
+                tempMax = {this.state.tempMax} mUnits = {this.state.isCelsius} /> 
                 
                 }
+
                 
             </div>
-               
+            
+                
+            
+              
         )
     }
 }
